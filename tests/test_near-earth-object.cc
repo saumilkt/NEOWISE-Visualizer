af443b41aa9a08f3e6e824383d783c50b0aa1dde
Saumil Thakore <saumilkt@gmail.com>
2020-11-17 21:52:12 -0800

    added getter tests for neobject

--- tests/test_near-earth-object.cc
+++ tests/test_near-earth-object.cc
@@ -17,3 +17,48 @@ TEST_CASE("Default Constructor") {
   REQUIRE(object.GetPHA() == "");
   REQUIRE(object.GetOrbitClass() == "");
 }
+
+TEST_CASE("Getters") {
+  neowisejson::NEObject object("designation","ddate",3,4,5,6,7,8,"pha","oclass");
+  SECTION("GetDesignation") {
+    REQUIRE(object.GetDesignation()=="desingation");
+  }
+
+  SECTION("GetDiscoveryDate") {
+    REQUIRE(object.GetDiscoverDate()=="ddate");
+  }
+
+  SECTION("GetHMag") {
+    REQUIRE(object.GetHMag()==3);
+  }
+
+  SECTION("GetMOID") {
+    REQUIRE(object.GetMOID()==4);
+  }
+
+  SECTION("GetQAU1") {
+    REQUIRE(object.GetQAU1()==5);
+  }
+
+  SECTION("GetQAU2") {
+    REQUIRE(object.GetQAU2()==6);
+  }
+
+  SECTION("GetPeriodYr") {
+    REQUIRE(object.GetPeriodYr()==7);
+  }
+
+  SECTION("GetIDeg") {
+    REQUIRE(object.GetIDeg()==8);
+  }
+
+  SECTION("GetPHA") {
+    REQUIRE(object.GetPHA()=="pha");
+  }
+
+  SECTION("GetOrbitClass") {
+    REQUIRE(object.GetOrbitClass()=="oclass");
+  }
+}
+
+
