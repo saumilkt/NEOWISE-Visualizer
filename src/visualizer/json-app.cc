0403bc6a59d3329753680a0a168e20707dd303a3
Saumil Thakore <saumilkt@gmail.com>
2020-12-01 13:51:17 -0800

    added visualizer declarations

---
+++ src/visualizer/json-app.cc
@@ -0,0 +1,25 @@
+//
+// Created by Saumil Thakore on 11/3/20.
+//
+
+#include "visualizer/json-app.h"
+#include "core/json-deserialize.h"
+namespace neowisejson {
+
+  namespace visualizer {
+
+    JsonApp::JsonApp() {
+      explorer_ = Explorer(neowisejson::DeserializeDataset("data/main/neowise.json"));
+
+    }
+    void JsonApp::setup() {
+      ci::app::setWindowSize((int) kWindowSize,(int) kWindowSize);
+    }
+
+    void JsonApp::draw() {
+      // first, clear the screen
+      ci::gl::clear(ci::Color::gray(0.1f));
+    }
+  } // namespace visualizer
+
+} // namespace idealgas
