134f3431103336715975081d1dfa32a865b4ae8b
Saumil Thakore <saumilkt@gmail.com>
2020-12-09 21:40:20 -0800

    rearranged function declarations for clarity

--- src/core/json-analyze.cc
+++ src/core/json-analyze.cc
@@ -25,29 +25,6 @@ namespace neowisejson {
     return NEObject();
   }
 
-  //analyze i_deg
-  std::string neowisejson::Explorer::GetObjectWithMaxIDeg(
-          double max_inclination) {
-    NEObject highest_inclination = objects_.at(0);
-    // check if object's q_au_1 value is greater than the current max's but
-    // still less than user-specified max
-    for (NEObject object : objects_) {
-      if (object.GetIDeg() > highest_inclination.GetIDeg() &&
-          object.GetIDeg() < max_inclination) {
-        highest_inclination = object;
-      }
-    }
-    return highest_inclination.GetDesignation();
-  }
-
-  double neowisejson::Explorer::GetAvgIDeg() {
-    double total_i_deg = 0;
-    for (NEObject object : objects_) {
-      total_i_deg += object.GetIDeg();
-    }
-    return total_i_deg / objects_.size();
-  }
-
   // analyze h_mag
   neowisejson::NEObject neowisejson::Explorer::GetObjectWithMaxHMag(
           double h_mag) {
@@ -111,26 +88,53 @@ namespace neowisejson {
     return total_moid / (double)total_objects;
   }
 
-  // analyze orbit_class
-  std::vector<NEObject> neowisejson::Explorer::FilterByOrbitClass(
-          string orbit_class) {
-    std::vector<NEObject> orbits;
+  // analyze q_au_1
+  std::string neowisejson::Explorer::GetObjectWithMaxPerihelion(
+          double max_perihelion) {
+    // create a holder neobject to represent the desired neobject and set it as
+    // the first object in the list
+    NEObject max_perihelion_object;
+    max_perihelion_object = objects_.at(0);
+    // check if object's q_au_1 value is greater than the current max's but
+    // still less than user-specified max
     for (NEObject object : objects_) {
-      if (object.GetOrbitClass() == orbit_class){
-        orbits.push_back(object);
+      if (object.GetQAU1() > max_perihelion_object.GetQAU1() &&
+          object.GetQAU1() < max_perihelion) {
+        max_perihelion_object = object;
       }
     }
-    return orbits;
+    return max_perihelion_object.GetDesignation();
   }
 
-  size_t neowisejson::Explorer::GetOrbitClassCount(string orbit_class) {
-    size_t orbit_count = 0;
+  double neowisejson::Explorer::GetAvgQAU1() {
+    double total_qau1 = 0;
     for (NEObject object : objects_) {
-      if (object.GetOrbitClass() == orbit_class){
-        orbit_count++;
+      total_qau1 += object.GetQAU1();
+    }
+    return total_qau1 / objects_.size();
+  }
+
+  // analyze q_au_2
+  std::string neowisejson::Explorer::GetObjectWithMaxApohelion(
+          double max_apohelion) {
+    NEObject highest_qau2 = objects_.at(0);
+    // check if object's q_au_1 value is greater than the current max's but
+    // still less than user-specified max
+    for (NEObject object : objects_) {
+      if (object.GetQAU2() > highest_qau2.GetQAU2() &&
+          object.GetQAU2() < max_apohelion) {
+        highest_qau2 = object;
       }
     }
-    return orbit_count;
+    return highest_qau2.GetDesignation();
+  }
+
+  double neowisejson::Explorer::GetAvgQAU2(){
+    double total_qau2 = 0;
+    for (NEObject object : objects_) {
+      total_qau2 += object.GetQAU2();
+    }
+    return total_qau2 / objects_.size();
   }
 
   // analyze period_yr
@@ -154,6 +158,29 @@ namespace neowisejson {
       total_period += object.GetPeriodYr();
     }
     return total_period / objects_.size();
+  }
+
+  //analyze i_deg
+  std::string neowisejson::Explorer::GetObjectWithMaxIDeg(
+          double max_inclination) {
+    NEObject highest_inclination = objects_.at(0);
+    // check if object's q_au_1 value is greater than the current max's but
+    // still less than user-specified max
+    for (NEObject object : objects_) {
+      if (object.GetIDeg() > highest_inclination.GetIDeg() &&
+          object.GetIDeg() < max_inclination) {
+        highest_inclination = object;
+      }
+    }
+    return highest_inclination.GetDesignation();
+  }
+
+  double neowisejson::Explorer::GetAvgIDeg() {
+    double total_i_deg = 0;
+    for (NEObject object : objects_) {
+      total_i_deg += object.GetIDeg();
+    }
+    return total_i_deg / objects_.size();
   }
 
   // analyze pha
@@ -180,51 +207,25 @@ namespace neowisejson {
     return pha_count;
   }
 
-  // analyze q_au_1
-  std::string neowisejson::Explorer::GetObjectWithMaxPerihelion(
-          double max_perihelion) {
-    // create a holder neobject to represent the desired neobject and set it as
-    // the first object in the list
-    NEObject max_perihelion_object;
-    max_perihelion_object = objects_.at(0);
-    // check if object's q_au_1 value is greater than the current max's but
-    // still less than user-specified max
+  // analyze orbit_class
+  std::vector<NEObject> neowisejson::Explorer::FilterByOrbitClass(
+          string orbit_class) {
+    std::vector<NEObject> orbits;
     for (NEObject object : objects_) {
-      if (object.GetQAU1() > max_perihelion_object.GetQAU1() &&
-          object.GetQAU1() < max_perihelion) {
-        max_perihelion_object = object;
+      if (object.GetOrbitClass() == orbit_class){
+        orbits.push_back(object);
       }
     }
-    return max_perihelion_object.GetDesignation();
+    return orbits;
   }
 
-  double neowisejson::Explorer::GetAvgQAU1() {
-    double total_qau1 = 0;
+  size_t neowisejson::Explorer::GetOrbitClassCount(string orbit_class) {
+    size_t orbit_count = 0;
     for (NEObject object : objects_) {
-      total_qau1 += object.GetQAU1();
-    }
-    return total_qau1 / objects_.size();
-  }
-  // analyze q_au_2
-  std::string neowisejson::Explorer::GetObjectWithMaxApohelion(
-          double max_apohelion) {
-    NEObject highest_qau2 = objects_.at(0);
-    // check if object's q_au_1 value is greater than the current max's but
-    // still less than user-specified max
-    for (NEObject object : objects_) {
-      if (object.GetQAU2() > highest_qau2.GetQAU2() &&
-          object.GetQAU2() < max_apohelion) {
-        highest_qau2 = object;
+      if (object.GetOrbitClass() == orbit_class){
+        orbit_count++;
       }
     }
-    return highest_qau2.GetDesignation();
-  }
-
-  double neowisejson::Explorer::GetAvgQAU2(){
-    double total_qau2 = 0;
-    for (NEObject object : objects_) {
-      total_qau2 += object.GetQAU2();
-    }
-    return total_qau2 / objects_.size();
+    return orbit_count;
   }
 } // namespace neowisejson
